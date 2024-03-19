8f298ffe06dee590d81f1359c620794c6f449e81
Dhaxx <kaiopablo44@gmail.com>
2024-03-19 09:05:24 -0300

    Alterando o model GiftCart

--- weddingSite/models/giftCart.py
+++ weddingSite/models/giftCart.py
@@ -1,9 +1,8 @@
 from weddingSite.models import *
 
 class GiftCart(models.Model):
-    guest = models.ForeignKey(Guest, on_delete=models.SET_NULL, null=True)
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE, null=True, related_name='gift_cart')
     gift = models.ForeignKey(Gift, on_delete=models.SET_NULL, null=True)
-    total = models.FloatField()
 
     def __str__(self):
         return self.guest.name if self.guest else 'None'
