5b262076cb2122389e1f4eb9a837189b47629ec9
Dhaxx <kaiopablo44@gmail.com>
2024-03-19 10:38:22 -0300

    Adicionando total ao carrinho de presentes

--- weddingSite/models/giftCart.py
+++ weddingSite/models/giftCart.py
@@ -2,11 +2,24 @@ from weddingSite.models import *
 
 class GiftCart(models.Model):
     guest = models.ForeignKey(Guest, on_delete=models.CASCADE, null=True, related_name='gift_cart')
-    gift = models.ForeignKey(Gift, on_delete=models.SET_NULL, null=True)
+    gifts = models.ManyToManyField('Gift', related_name='gift_cart')
 
     def __str__(self):
         return self.guest.name if self.guest else 'None'
     
+    def total(self):
+        return sum([gift.price for gift in self.gifts.all()])
+
+class GiftCartInline(admin.TabularInline):
+    model = GiftCart.gifts.through
+    
 class GiftCartAdmin(admin.ModelAdmin):
+    def total(self, obj):
+        return obj.total()
+    total.short_description = 'Total'
+
+    inlines = [GiftCartInline] if GiftCart.gifts.through not in admin.site._registry else []
+
     list_display = ('id', 'guest', 'total',)
     list_display_links = ('id', 'guest',)
+    exclude = 'gifts',
