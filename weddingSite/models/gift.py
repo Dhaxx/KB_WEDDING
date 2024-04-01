86e67c3af41d2727294e58100302dba476e65817
Dhaxx <kaiopablo44@gmail.com>
2024-04-01 09:15:10 -0300

    Removendo campo de valor do model gift

--- weddingSite/models/gift.py
+++ weddingSite/models/gift.py
@@ -4,7 +4,6 @@ class Gift(models.Model):
     image = models.ImageField(upload_to='gifts', null=False, blank=False)
     name = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
-    price = models.FloatField()
     link = models.CharField(max_length=255, null=True, blank=True)
     status = models.BooleanField(default=False)
 
@@ -12,5 +11,5 @@ class Gift(models.Model):
         return self.name
     
 class GiftAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'status',)
+    list_display = ('id', 'name', 'status',)
     list_display_links = ('id', 'name',)
