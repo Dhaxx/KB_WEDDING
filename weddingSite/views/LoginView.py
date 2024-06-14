fae3fbda83fd07f13b757ae6bad54dbc6b990c52
Dhaxx <kaiopablo44@gmail.com>
2024-06-14 16:33:18 -0300

    Adicionado Django-axes para bloquear tentativas de login

--- weddingSite/views/LoginView.py
+++ weddingSite/views/LoginView.py
@@ -1,6 +1,9 @@
-from django.shortcuts import redirect
-from weddingSite.views import *
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from axes.handlers.proxy import AxesProxyHandler
+from axes.utils import reset
 from weddingSite.forms import LoginForm
+from weddingSite.models import GroupGuest  # Certifique-se de importar o modelo correto
 
 def login_view(request):
     if request.method == "POST":
@@ -9,6 +12,9 @@ def login_view(request):
             token = form.cleaned_data.get('token')
             group_guest = GroupGuest.objects.filter(token=token).first()
             if group_guest:
+                # Resetar tentativas falhadas em caso de sucesso
+                reset(request)
+                
                 request.session['group_guest_id'] = group_guest.id
                 if group_guest.status in [0, 2]:
                     request.session['show_modal'] = True
@@ -16,6 +22,11 @@ def login_view(request):
                     request.session['show_modal'] = False
                 return redirect('home')  # substitua 'home' pelo nome da sua URL de destino
             else:
+                # Registrar tentativa falhada
+                credentials = {
+                    'username': 'invalid_token',
+                }
+                AxesProxyHandler.user_login_failed(sender=None, request=request, credentials=credentials)
                 messages.error(request, 'Token Inválido! Tente Novamente.')
     else:
         form = LoginForm()
